4019c44a05a8b1097eb17ce84add6e3f6a7bbd0e
Aaditya Banchhiwal <aaditya280898@gmail.com>
2018-10-03 13:06:27 +0530

    Test2

--- app/src/main/java/breathe/inventerous/com/breathe/MainActivity.java
+++ app/src/main/java/breathe/inventerous/com/breathe/MainActivity.java
@@ -1,7 +1,9 @@
 package breathe.inventerous.com.breathe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -29,5 +31,14 @@ public class MainActivity extends AppCompatActivity {
         btnGoogle=(Button) findViewById(R.id.btnGoogle);
         btnFb=(Button) findViewById(R.id.btnFb);
         btnTwitter=(Button) findViewById(R.id.btnTwitter);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent1);
+                finish();
+            }
+        });
     }
 }
