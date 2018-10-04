9dad63772e353d1454f9472ec78e0591e78b9ed6
Kriti Chopra <37541206+kriti-chopra@users.noreply.github.com>
2018-10-05 00:27:49 +0530

    test1/kriti

--- app/src/main/java/breathe/inventerous/com/breathe/AccDetails.java
+++ app/src/main/java/breathe/inventerous/com/breathe/AccDetails.java
@@ -3,17 +3,20 @@ package breathe.inventerous.com.breathe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AccDetails extends AppCompatActivity {
-    private Button btnShop, btnForecast, btnSearchLoc;
+    TextView txtName,txtEmailId,txtPhoneNo,txtLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acc_details);
 
-        btnForecast=(Button) findViewById(R.id.btnForecast);
-        btnSearchLoc=(Button) findViewById(R.id.btnSearchLoc);
-        btnShop=(Button) findViewById(R.id.btnShop);
+        txtName=(TextView) findViewById(R.id.txtName);
+        txtEmailId=(TextView) findViewById(R.id.txtEmailId);
+        txtLocation=(TextView) findViewById(R.id.txtLocation);
+        txtPhoneNo=(TextView) findViewById(R.id.txtPhoneNo);
+
     }
 }
--- app/src/main/java/breathe/inventerous/com/breathe/MainActivity.java
+++ app/src/main/java/breathe/inventerous/com/breathe/MainActivity.java
@@ -1,12 +1,21 @@
 package breathe.inventerous.com.breathe;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnSignUp;
     private Button btnSignIn;
     private Button btnGoogle,btnFb,btnTwitter;
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -32,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
         btnFb=(Button) findViewById(R.id.btnFb);
         btnTwitter=(Button) findViewById(R.id.btnTwitter);
 
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        //if user already signed in then go to actual interface directly
+        if(firebaseAuth.getCurrentUser()!=null){
+            Intent intentToMain=new Intent(MainActivity.this,Actual_Interface.class);
+            startActivity(intentToMain);
+            finish();
+        }
+
+        //going to sign up page but will sign up in sign up activity
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,5 +60,37 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        //signing in with Email and password
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String myEmail= edtEmail.getText().toString();
+                String myPassword=edtPassword.getText().toString();
+                signInWithEmailAndPassword(myEmail,myPassword);
+            }
+        });
     }
+
+    //signing in function
+    private void signInWithEmailAndPassword(String userEmail,String userPassword){
+
+        firebaseAuth.signInWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(!task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "There was an error", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //after signing in will go to actual interface
+                    Intent intentToMain=new Intent(MainActivity.this,Actual_Interface.class);
+                    startActivity(intentToMain);
+                    finish();
+                }
+            }
+        });
+    }
+
 }
