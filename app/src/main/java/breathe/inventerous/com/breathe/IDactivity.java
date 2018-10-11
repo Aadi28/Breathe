e24b5e6aff163eb7e98c2b25b1d0cb16cb837af8
Jatin Bhardwaj <31664635+jatinbharadwaj@users.noreply.github.com>
2018-10-11 17:26:48 +0530

    Location/Latitude/Longitude

--- app/src/main/java/breathe/inventerous/com/breathe/IDactivity.java
+++ app/src/main/java/breathe/inventerous/com/breathe/IDactivity.java
@@ -1,13 +1,22 @@
 package breathe.inventerous.com.breathe;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +35,11 @@ public class IDactivity extends AppCompatActivity {
 
     private String myId;
     private final String REF_To_ID_CHILD="RefToIDChild";
+    private LocationManager locationManager;
+    private LocationListener listener;
+    private double longX;
+    private double latY;
+    private static final int REQ_CODE=10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +71,36 @@ public class IDactivity extends AppCompatActivity {
             public void onClick(View view) {
                 myId=edtID.getText().toString();
 
+                locationManager=(LocationManager) getSystemService(LOCATION_SERVICE);
+
+                listener= new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        longX=location.getLongitude();
+                        latY=location.getLatitude();
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                        Intent toPermission=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(toPermission);
+                    }
+
+                };
+
+                configure_button();
+
+
                 databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -78,7 +122,7 @@ public class IDactivity extends AppCompatActivity {
                             //we will pass on these reference for myId child to sign up
                             //after creating the other fields
                             //later we will add values to these fields
-                            idChild.child("Location").setValue("No location yet");
+                            //idChild.child("Location").setValue("No location yet");
 //                            idChild.child("Email");
 //                            idChild.child("Password");
 //                            idChild.child("Readings");
@@ -112,5 +156,56 @@ public class IDactivity extends AppCompatActivity {
 //
 //            }
 //        });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+
+            case REQ_CODE:
+
+                configure_button();
+
+                break;
+
+            default:
+
+                break;
+
+        }
+    }
+
+    private void configure_button(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+
+                        ,REQ_CODE);
+
+            }
+
+            return;
+        }
+        else{
+            locationManager.requestLocationUpdates("gps",5000,0,listener);
+            //idChild.child("Location").setValue("No location yet");
+
+            Toast.makeText(IDactivity.this, "X"+ longX, Toast.LENGTH_SHORT).show();
+            Toast.makeText(IDactivity.this, "Y"+ latY, Toast.LENGTH_SHORT).show();
+//            databaseReference.addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                   dataSnapshot.getChildren().;
+//                }
+//
+//                @Override
+//                public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//                }
+//            });
+        }
     }
 }
